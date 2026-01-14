94fab5f164a3162aed0b0e3eb93972900ec08a8d
tanishmundra-codes <tanishmundra2@gmail.com>
2026-01-14 19:52:16 +0530

    add get all categories with pagination

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -2,8 +2,10 @@ const Category = require("../models/category.model");
 
 async function createCategory(req, res) {
     try {
+        // Read input
         const { name, image, description, tax_applicable, tax_percentage, is_active } = req.body;
 
+        // Apply logic
         const newCategory = new Category({
             name,
             image,
@@ -12,9 +14,11 @@ async function createCategory(req, res) {
             tax_percentage,
             is_active
         });
-
+        
+        // Query DB
         const savedCategory = await newCategory.save();
-
+        
+        // Return response
         return res.status(201).json({
             success: true,
             data: savedCategory,
@@ -37,6 +41,45 @@ async function createCategory(req, res) {
     }
 }
 
+async function getAllCategories(req, res) {
+    try {
+        let { limit, page } = req.query;
+
+        if (!page) {
+            page = 1;
+        }
+        if (!limit) {
+            limit = 10;
+        }
+
+        limit = Number(limit);
+        page = Number(page);
+
+        let skip = (page - 1) * limit
+
+        const categories = await Category.find({})
+            .skip(skip)
+            .limit(limit)
+
+        const total = await Category.countDocuments();
+
+        return res.status(200).json({
+            success: true,
+            page,
+            limit,
+            total,
+            data: categories,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Server Error",
+            error: error.message,
+        });
+    }
+}
+
 module.exports = {
     createCategory,
+    getAllCategories
 };
