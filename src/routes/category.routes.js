73ca67e836d2448a1cd3b89b54ed321fd101c699
tanishmundra-codes <tanishmundra2@gmail.com>
2026-01-14 19:51:55 +0530

    add get all categories with pagination

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const {createCategory} = require("../controller/category.controller");
+const {createCategory, getAllCategories} = require("../controller/category.controller");
 const router = express.Router();
 
 router.post("/categories", createCategory)
+router.get("/categories", getAllCategories)
 
 module.exports = router;
