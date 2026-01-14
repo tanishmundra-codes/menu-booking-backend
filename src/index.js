9f37416876488b871c7d4c188501aa02fcb6b58d
tanishmundra-codes <tanishmundra2@gmail.com>
2026-01-14 16:53:03 +0530

    create api for create categories

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const Joi = require("joi")
+const categoryRoute = require("./routes/category.routes")
 
 const app = express();
+const category = require("./controller/category.controller");
 
 app.use(express.json());
+app.use("/", categoryRoute);
 
 module.exports = app;
