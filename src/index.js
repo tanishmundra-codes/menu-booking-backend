91da2e3823ca36fe8d6a8dc5f4cd48382734ac7c
tanishmundra-codes <tanishmundra2@gmail.com>
2026-01-15 23:08:00 +0530

    pricing service addedd for discount

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const Joi = require("joi")
 const categoryRoute = require("./routes/category.routes")
 const subcategoryRoutes = require("./routes/subcategory.routes");
+const itemRoutes = require("./routes/item.routes");
+const priceRoutes = require("./routes/price.routes");
 
 const app = express();
-const category = require("./controller/category.controller");
+
 
 app.use(express.json());
 app.use("/", categoryRoute);
 app.use(subcategoryRoutes);
+app.use(itemRoutes);
+app.use(priceRoutes);
 
 module.exports = app;
