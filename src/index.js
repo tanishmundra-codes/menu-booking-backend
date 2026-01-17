69e560985469ddf14347c4ce8c8c2a17d044813a
tanishmundra-codes <tanishmundra2@gmail.com>
2026-01-17 11:20:55 +0530

    addons added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const itemRoutes = require("./routes/item.routes");
 const priceRoutes = require("./routes/price.routes");
 const availabilityRoutes = require("./routes/availability.routes");
 const bookingRoutes = require("./routes/booking.routes");
+const addonsRoutes = require("./routes/addons.routes");
 
 const app = express();
 
@@ -17,5 +18,6 @@ app.use(itemRoutes);
 app.use(priceRoutes);
 app.use(availabilityRoutes);
 app.use(bookingRoutes);
+app.use(addonsRoutes);
 
 module.exports = app;
